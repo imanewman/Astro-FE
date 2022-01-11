7018bb9376ddb45b79b11f20dc4a277f79cbbf2d
Tim <tjnewman111@gmail.com>
2022-01-10 21:01:11 -0500

    added points by type

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -12,4 +12,52 @@ export const storableEventTypes: EventType[] = [
   "Election",
 ];
 
+export const pointsByType: Record<string, Point[]> = {
+  Visible: [
+    "Moon",
+    "Mercury",
+    "Venus",
+    "Sun",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+  ],
+  Outer: [
+    "Uranus",
+    "Neptune",
+    "Pluto",
+  ],
+  Nodes: [
+    "True North Node",
+    "True South Node",
+  ],
+  Asteroids: [
+    "Ceres",
+    "Pallas",
+    "Juno",
+    "Vesta",
+  ],
+  Centaurs: [
+    "Chiron",
+    "Pholus",
+    "Charklo",
+  ],
+  Points: [
+    "Ascendant",
+    "Midheaven",
+    "Descendant",
+    "Inner Heaven",
+    "Vertex",
+  ],
+  Lots: [
+    "Lot of Fortune",
+    "Lot of Spirit",
+    "Lot of Necessity",
+    "Lot of Eros",
+    "Lot of Courage",
+    "Lot of Victory",
+    "Lot of Nemesis",
+  ],
+};
+
 export default { dateFormat, eventTypes: storableEventTypes };
